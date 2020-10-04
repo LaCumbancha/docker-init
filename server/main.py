753b2b5a525589b7d26eb3a69f56412aae838497
Cristian Raña <cristian.rana@despegar.com>
2020-10-04 20:41:41 -0300

    Server configuration loaded by file

--- server/main.py
+++ server/main.py
@@ -2,6 +2,7 @@
 
 import os
 import time
+import yaml
 import logging
 from common.server import Server
 
@@ -15,14 +16,31 @@ def parse_config_params():
 	be parsed, a ValueError is thrown. If parsing succeeded, the function
 	returns a map with the env variables
 	"""
+
 	config_params = {}
-	try:
+	if "SERVER_CONFIG_FILE" in os.environ:
+		try:
+			config_file_name = os.environ["SERVER_CONFIG_FILE"]
+
+			config_file = open(config_file_name)
+			config_params = yaml.load(config_file, Loader=yaml.FullLoader)
+		except FileNotFoundError as e:
+			raise Exception(f"Config file {config_file_name} not found. Aborting server.")
+		except yaml.MarkedYAMLError as e:
+			raise Exception(f"Couldn't load config file {config_file_name}. Aborting server.")
+
+	if "SERVER_PORT" in os.environ:
 		config_params["port"] = int(os.environ["SERVER_PORT"])
+
+	if "SERVER_LISTEN_BACKLOG" in os.environ:
 		config_params["listen_backlog"] = int(os.environ["SERVER_LISTEN_BACKLOG"])
-	except KeyError as e:
-		raise KeyError("Key was not found. Error: {} .Aborting server".format(e))
-	except ValueError as e:
-		raise ValueError("Key could not be parsed. Error: {}. Aborting server".format(e))
+
+	"""Checking for missing variables."""
+	if "port" not in config_params:
+		raise Exception("Missing variable SERVER_PORT. Aborting server.")
+
+	if "listen_backlog" not in config_params:
+		raise Exception("Missing variable SERVER_LISTEN_BACKLOG. Aborting server.")
 
 	return config_params
 
